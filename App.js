74f0adfb5a620ecccf7c3911129ebf938458b4bc
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-28 11:32:34 -0400

    Initial commit for navigation bar

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Name from './src/components/MatchCards/Name.js'
+
+import Navigation from './src/shared/Navigation.js';
+import Name from './src/components/MatchCards/Name.js';
 
 export default function App() {
   return (
-    <Name/>
+    <View>
+      <Navigation/>
+      <Name/>
+    </View>
   );
 }
 
---
+++ src/shared/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function Navigation() {
+  return(
+    <View>
+
+    </View>
+  );
+}
