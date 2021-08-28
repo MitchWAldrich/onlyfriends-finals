cb3040fb0c5a12d53ec537cc6eb1c07d1eeee6bd
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-28 14:53:01 -0400

    add line for sign up page

--- Login.js
+++ Login.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 import LoginForm from './src/components/LoginForm';
 
 export default function Login() {
+
   return (
     <View style={styles.container}>
-      <Text> Login </Text>
+      <Text> onlyFriends </Text>
       <LoginForm />
+      <Text>
+        Not a friend yet? 
+        <Button 
+        title="Sign up"
+        onPress={() => Alert.alert('Reroute to sign up')} />
+      </Text>
     </View>
   );
 }
