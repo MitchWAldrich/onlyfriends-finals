862bb9b5b3b2ae21388b7e84759792e816832478
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-09-01 19:21:46 -0400

    add save and getData to persist with asyncstorage

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,13 +2,13 @@ import React, { useState, useContext, useEffect } from "react";
 import { SafeAreaView, TextInput, Button, Alert, StyleSheet } from "react-native";
 import { StateContext } from '../../StateProvider.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+import { save, getData } from '../helpers/persistLogin.js';
 
 
 export default function LoginForm() {
   // const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [name, setName] = useState();
   
   const { state, setState, auth, setAuth, email, setEmail } = useContext(StateContext)
 
@@ -17,60 +17,25 @@ export default function LoginForm() {
   const users = state.users;
 
   const onSubmit = async (event) => {
-    // event.preventDefault();
-
     for (const user of users) {
-      if (email === user.email) {
-        if (password === user.password) {
-          try {
-            // setState(prev => ({...prev, user}));
-            // const jsonValue = JSON.stringify(user)
-            await AsyncStorage.setItem('MyEmail', email)
-          } catch (err) {
-            console.log(err) 
-          }
+        if (email === user.email) {
+            if (password === user.password) {
+
+              save(user, auth);
+              getData(state, user, setState, auth, setAuth);
           
-          return
+            return
           
         }
       }
       
     }
   };
-
-   const getData = async () => {
-            try {
-              console.log('email', email)
-              // const jsonValue = await AsyncStorage.getItem('user')
-              const email = await AsyncStorage.getItem('MyEmail')
-              if (email !== null) {
-                // parsedValue = JSON.parse(user)
-                setEmail(( email ))
-                setAuth((true))
-                console.log('auth2', auth)
-              }
-            } catch (e) {
-              console.log(e)
-              // error reading value
-            }
-          }
-
-    const remove = async () => {
-      try {
-        await AsyncStorage.removeItem('MyEmail')
-
-      } catch (err) {
-        
-        alert(err)
-
-      } finally {
-        setEmail("")
-      }
-    }
   
   useEffect(() => {
     getData();
   }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <TextInput
@@ -85,9 +50,6 @@ export default function LoginForm() {
       <Button
         title="Login"
         onPress={onSubmit} />
-        {/* // onPress={() => navigation.navigate('MyTabs')} /> */}
-
-    {/* onPress={() => navigation.navigate('Chat', {userName: item.userName})} */}
     </SafeAreaView>
   );
 }
