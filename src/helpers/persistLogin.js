fe6dcae4bef0962f7c688890a1ef1a8e1bfd402a
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-09-01 19:25:48 -0400

    add save, getData, remove for async storage

---
+++ src/helpers/persistLogin.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, useContext } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StateContext } from '../../StateProvider.js';
+
+
+export const save = async (user, auth) => {
+
+        try {
+
+          await AsyncStorage.setItem('MyUser', JSON.stringify(user))
+          await AsyncStorage.setItem('MyAuth', auth)
+
+        } catch (err) {
+
+          console.log(err) 
+
+        }        
+}
+
+export const getData = async (state, user, setState, auth, setAuth) => {
+
+  try {
+
+    const jsonValue = await AsyncStorage.getItem('MyUser')
+    auth = await AsyncStorage.getItem('MyAuth')
+
+    if (jsonValue !== null) {
+
+      const parsedUser = JSON.parse(jsonValue)
+      setState(({...state, user: parsedUser }))
+      setAuth((true))
+
+    }
+    
+  } catch (e) {
+    // error reading value
+    console.log(e)
+  }
+}
+
+export const remove = async (state, setState, auth, setAuth) => {
+  try {
+
+    await AsyncStorage.removeItem('MyUser')
+    
+  } catch (err) {
+    
+    alert(err)
+  } finally {
+
+    setState({...state, user: null});
+    setAuth(false);
+
+  }
+}
