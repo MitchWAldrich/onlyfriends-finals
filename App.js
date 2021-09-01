ce76c82eec1d4d9976100c030a257a1d308faaa5
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-09-01 19:21:08 -0400

    change ProfileStack to ProfileScreen in MyTabs

--- App.js
+++ App.js
@@ -4,17 +4,16 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import 'react-native-gesture-handler';
 
-import StateProvider from './StateProvider';
+import StateProvider from './StateProvider.js';
 import AppStack from './src/navigation/AppStack';
 
 export default function App() {
-  
-  return (
-    <NavigationContainer>
-      <StateProvider>
-        <AppStack/>
-      </StateProvider>
-    </NavigationContainer>
-  );
-}
 
+return (
+  <NavigationContainer>
+    <StateProvider>
+      <AppStack />
+    </StateProvider>
+  </NavigationContainer>
+);
+}
