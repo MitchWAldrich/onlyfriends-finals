c85201be3b78907706874268599fbe3badfabdea
Beatrice <beatrice.c.kwan@gmail.com>
2021-09-01 22:14:49 -0400

    add functionality to step through each user

--- src/components/MatchCards/index.js
+++ src/components/MatchCards/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
 import { StateContext } from '../../../StateProvider';
 import { fullUserObject } from '../../helpers/selectors';
 
@@ -12,14 +12,31 @@ import Extras from './Extras';
 
 
 const Cards = function(props)  {
+  const [index, setIndex] = useState(0);
+
   const { state } = useContext(StateContext);
   const users = state.users;
   const user = state.user;
 
   const filteredUsers = users.filter(person => person.id !== user.id);
 
-  const displayedUser = fullUserObject({users: state.users, interests: state.interests, photos:state.photos}, filteredUsers[0])
   console.log('fUs', filteredUsers)
+  
+  
+  const detailedUsers = filteredUsers.map(person => fullUserObject({users: state.users, interests: state.interests, photos: state.photos}, person))
+  
+  let currentUser = detailedUsers[index];
+  console.log('detailedUsers', detailedUsers)
+  
+  let displayedUser = fullUserObject({users: state.users, interests: state.interests, photos: state.photos}, currentUser)
+  
+  const incrementUser = (index) => {
+    if (index === detailedUsers.length - 1) {
+      return setIndex(0)
+    }
+  
+    return setIndex(index += 1)
+  }
 
   //Card modes
   const NAME = 'NAME';
@@ -49,6 +66,7 @@ const Cards = function(props)  {
 
   return (
     <SafeAreaView style={styles.container}>
+      <Button title="New" onPress={() => incrementUser(index)} style={styles.navigate}/>
       {mode === NAME && <Name next={() => next(BIO)} detailedUser={displayedUser} /> }
       {mode === BIO && <Bio next={() => next(INTERESTS)} back={() => back(NAME)} detailedUser={displayedUser}/>}
       {mode === INTERESTS && <Interests next={() => next(EXTRAS)} back={() => back(BIO)} detailedUser={displayedUser}/>}
