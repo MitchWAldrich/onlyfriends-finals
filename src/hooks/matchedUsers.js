3845343e50647b576f2b5c4d54565d7f244b961e
Beatrice <beatrice.c.kwan@gmail.com>
2021-09-03 12:59:13 -0400

    add matched users hook to post new matches

---
+++ src/hooks/matchedUsers.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+
+export function matchedUsers(state, user1, user2) {
+      
+      return axios.post('http://localhost:8001/api/matches', {
+        user1_id: user1.id,
+        user2_id: user2.id,
+        best_friend: false
+      })
+        .then(() => {
+          console.log('I hope you match!')
+        })
+        .catch((e) => {
+          throw e
+        })
+  }
+
+
+
+  //     user1Matches.push(potentialMatch)
+  //   }
+  // }
+  
+  // const user2Matches = [];
+  // for (const potentialMatch of state.potential_matches) {
+  //   if (potentialMatch.user1_id === user2.id) {
+  //     user2Matches.push(potentialMatch)
+  //   }
+  // }
+  
+  // const match = [];
+  // for (const match1 of user1Matches) {
+    
+  //   for (const match2 of user2Matches) {
+  //     if (match1.user1_id === match2.user2_id && match2.user1_id === match1.user2_id) {
+  //       match.push(match1, match2)
+  //     }
+  //   }
+  // }
+  
+  // if (match.length === 0) {
+  //   return null;
+  // } else {
+    
+  //   return match
+  // }
